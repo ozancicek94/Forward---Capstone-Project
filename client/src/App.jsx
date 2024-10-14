46cfcd2bf2346eed04e00518e061d71665dac3ac
Ozan Cicek <ozancicek94@gmail.com>
2024-10-14 00:36:06 -0400

    test comment_29

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from "react-router-dom";
 import Courts from "./components/Courts";
 import SingleCourt from "./components/SingleCourt";
 import Login from "./components/Login";
@@ -16,16 +16,32 @@ function AppContent() {
   // Check if we are on the opening page
   const isOpeningPage = location.pathname === '/';
 
+  // Use effect to check token on page load
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    
+    if (token) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false); // Ensure the user is logged out initially
+    }
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem('token'); // Remove the token
+    setIsLoggedIn(false); // Update the logged-in state
+  };
+
   return (
     <div className={isOpeningPage ? 'opening-page' : 'blue-page'}>
-      <Navigations isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
+      <Navigations isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} handleLogout={handleLogout} />
       <Routes>
         <Route path='/' element={<OpeningPageApp />} />
-        <Route path='/Courts' element={<Courts />} />
-        <Route path='/courts/:id' element={<SingleCourt />} />
+        <Route path='/Courts' element={isLoggedIn ? <Courts /> : <Navigate to="/LogIn" />} />
+        <Route path='/courts/:id' element={isLoggedIn ? <SingleCourt /> : <Navigate to="/LogIn" />} />
         <Route path='/LogIn' element={<Login setIsLoggedIn={setIsLoggedIn} />} />
         <Route path='/Register' element={<Register />} />
-        <Route path='/Account' element={<Account />} />
+        <Route path='/Account' element={isLoggedIn ? <Account /> : <Navigate to="/LogIn" />} />
       </Routes>
     </div>
   );
