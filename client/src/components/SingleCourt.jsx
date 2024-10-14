0e9a75dcbad448adb57bcbd2cf89979cbb19253e
Ozan Cicek <ozancicek94@gmail.com>
2024-10-13 20:54:54 -0400

    test comment_19

--- client/src/components/SingleCourt.jsx
+++ client/src/components/SingleCourt.jsx
@@ -415,7 +415,7 @@ export default function SingleCourt() {
   
     try {
       const response = await fetch(
-        `https://forward-capstone-project.onrender.com//api/reviews/${editReviewId}`,
+        `https://forward-capstone-project.onrender.com/api/reviews/${editReviewId}`,
         {
           method: "PATCH",
           headers: {
